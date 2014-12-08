1a26df87bdae0b8ccc93047b535f87d9daa2bf84
craigweinschenk <craigweinschenk@d008c882-1ab9-dbc1-6f75-8481792a1962>
2014-12-08 03:29:49 +0000

    scripts: update incident script
    git-svn-id: http://fire-risk.googlecode.com/svn/trunk@22 d008c882-1ab9-dbc1-6f75-8481792a1962

--- Scripts/fire_incident.py
+++ Scripts/fire_incident.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pylab import *
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
+import random
 
 incident = pd.read_csv('../Data/arlington_incidents.csv', header=0)
 total_incidents = len(incident['incident_class_code'])
@@ -15,4 +16,10 @@ for i in incident['incident_class_code']:
 	if i == 1:
 	   total_fires = total_fires + 1 
 
-print 100*(total_fires/total_incidents)
+for num in range(1,1000):
+	rand = random.randrange(1,len(incident['incident_class_code']),1)
+	if incident['incident_class_code'][rand] == 1:
+	 	fire = True
+	 	print rand,fire
+	else:
+	 	fire = False
