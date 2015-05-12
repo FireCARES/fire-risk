572e1c20369e0c15a02560a611257e9866938819
Tyler Garner <garnertb@gmail.com>
2015-05-12 15:13:19 -0400

    Remove unused imports.

--- tests/models/tests.py
+++ tests/models/tests.py
@@ -1,8 +1,5 @@
 import unittest
-
 from fire_risk.models.DIST import DIST
-from fire_risk.backends import PostgresBackend
-from fire_risk.backends.queries import RESIDENTIAL_FIRES_BY_FDID_STATE
 
 
 class TestDISTModel(unittest.TestCase):
@@ -16,10 +13,6 @@ class TestDISTModel(unittest.TestCase):
                    'room_of_origin': 507378L,
                    'building_of_origin': 529300L}
 
-        # from fire_risk.backends import PostgresBackend
-        # with PostgresBackend(dict(host='localhost')) as backend:
-        #     results = backend.get_firespread_counts()
-        # print results
         dist = DIST(floor_extent=floor_extent, **results)
 
         self.assertAlmostEqual(dist.gibbs_sample(), 32.0, delta=4)
