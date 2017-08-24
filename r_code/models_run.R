62f16625910eb44dd7dbbd99ba20cb4c047884d0
Stanley Gilbert <stanley.gilbert@nist.gov>
2017-08-24 16:07:01 -0400

    Sets default runs

--- r_code/models_run.R
+++ r_code/models_run.R
@@ -1,8 +1,8 @@
-models.run <- matrix( c("npt.final",   "lr"
-                        "npt.final.L", "lr"
-                        "mr.final",    "mr"
-                        "hr.final",    "hr"                                                ),
+models.run <- matrix( c("lr", "npt.final",
+                        "lr", "npt.final.L",
+                        "mr", "mr.final",
+                        "hr", "hr.final"     ),
                       byrow=TRUE,
                       ncol=2 )
 models.run <- as.data.frame(models.run)
-names(models.run) <- c("lst", "risk")
+names(models.run) <- c("risk", "lst")
