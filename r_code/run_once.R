421cd1fc877fc6f88e58fdd051f7dbf5c55080a7
Stanley Gilbert <stanley.gilbert@nist.gov>
2017-08-24 14:26:04 -0400

    Added comments and install census api key

--- r_code/run_once.R
+++ r_code/run_once.R
@@ -1,3 +1,4 @@
+# First, install all packages needed to run pkgFireCARES and the associated scripts
 install.packages( c( "RPostgreSQL",
                      "ranger",
                      "glmnet",
@@ -8,7 +9,14 @@ install.packages( c( "RPostgreSQL",
                      "utils"), 
                   repos="https://cloud.r-project.org",
                   dependencies=c("Depends","Imports") )
+# Next compile the pkgFireCARES package. I need to know if devtools requires the  
+# RTools keychain installed. Since the server is running on a Linux box, this is 
+# probably not a current problem. But this needs to be tested to be sure.
 library(devtools)
 library(roxygen2)
 document("pkgFireCARES")
 install ("pkgFireCARES")
+# install the Census key
+# Key needs to be requested at http://api.census.gov/data/key_signup.html
+library(acs)
+api.key.install( "d075012434f632cf410c8b7d9441c0f56747117c" )
