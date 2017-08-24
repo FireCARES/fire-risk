69d2ea0605429f8e8bda96abc06caf013c753a1f
Stanley Gilbert <stanley.gilbert@nist.gov>
2017-08-24 16:48:04 -0400

    Complete rewrite

--- r_code/full_analysis.R
+++ r_code/full_analysis.R
@@ -1,91 +1,148 @@
-# source_data     source_table    model_s   est_data   est_table  rollup
-# low.risk.fires  low_risk_fires  npt.final lr.mr.pred lr_mr_pred FALSE
-# med.risk.fires  med_risk_fires  mr.final  lr.mr.pred lr_mr_pred FALSE
-# high.risk.fires high_risk_fires hr.final  hr.pred    hr_pred    TRUE
+# full_analysis.R
 #
+# This script is called as:
+# source("full_analysis.R")
 #
+# There are three switches that this uses to determine its behavior:
+# 
+# * The data.frame: models.run. This data frame determines what models are run.
+#   Its format is given as an example below. If it is undefined, then a default
+#   version is loaded from 'models_run.R' Note that multiple model objects per
+#   risk level does not present a problem.
 #
+#       risk lst
+#       lr   npt.final
+#       lr   npt.final.L
+#       mr   mr.final
+#       hr   hr.final
 #
+# * The logical variable 'do.predictions'. If it is 
+#   TRUE, then predictions are generated from the models run. If not, then no predictions
+#   are generated from the estimated models. If it is undefined, then it is assumed to be TRUE.
+#
+# * The logical variable 'roll.up.2.dept'. If it is TRUE, then the predictions are rolled
+#   up to the department level. If it is FALSE, then the predictions are left at the census
+#   tract level. If it is undefined, then it is assumed to be FALSE.
+#
+# As currently written, it is assumed that the caller can define these variables in the 
+# Global Environment before 'source'ing this script. If not, then some adjustment will 
+# have to be made.
+#
+# Future Work:
+# * I do not like the data.frame approach to models.run. My preference would be a list 
+#   that looks something like this:
+#
+#   list( lr=c("npt.final", "npt.final.L"),
+#         mr=c("mr.final"),
+#         hr=c("hr.final")
+#       )
+#   I like it better because it more cleanly represents what is being done and the work flow.
+#   If Python can handle R lists, then this becomes the preferred approach.
+#
+src.names <- c( lr="low.risk.fires",
+                mr="med.risk.fires",
+                hr="high.risk.fires")
+src.tabls <- c( lr="select * from nist.low_risk_fires",
+                mr="select * from nist.med_risk_fires",
+                hr="select * from nist.high_risk_fires")
+est.names <- c( lr="lr.mr.pred",
+                mr="lr.mr.pred",
+                hr="hr.pred")
+est.tabls <- c( lr="select * from nist.lr_mr_pred",
+                mr="select * from nist.lr_mr_pred",
+                hr="select * from nist.hr_pred")
 library(RPostgreSQL)
 library(pkgFireCARES)
-conn <- dbConnect( "PostgreSQL", 
-                   host="firecares-restore.c3gxdjk57saa.us-east-1.rds.amazonaws.com", 
-                   dbname="nfirs", 
-                   user="username", 
-                   password="pwd")
+if(! exists("conn")){
+  conn <- dbConnect( "PostgreSQL", 
+                     host="firecares-restore.c3gxdjk57saa.us-east-1.rds.amazonaws.com", 
+                     dbname="nfirs", 
+                     user="username", 
+                     password="pwd")
+}
+browser()
 # The 'models.run' object contains the list of control objects
 # to run and some key information this script needs to run them.
 # Check to see if it already exists. If not load it off the disk.
 # If it still doesn't exist, error out.
 if( ! exists("models.run"))   source("models_run.R")
 if( ! exists("models.run")) stop("The list of models to run (in 'models.run') does not exist!")
-if( 'lr' %in% models.run$risk ){
-  low.risk.fires  <- dbGetQuery(conn, "select * from nist.low_risk_fires" )
-  low.risk.fires  <- fcSetup(low.risk.fires )
-  lr.mr.pred <- dbGetQuery(conn, "select * from nist.lr_mr_pred")
-  lr.mr.pred <- fcSetup(lr.mr.pred)
+if(length(setdiff(unique(models.run$risk), c("lr", "mr", "hr"))) > 0) stop( "The only risk classes allowed in models.run are 'lr', 'mr', and 'hr'!")
+if( ! exists("roll.up.2.dept")) roll.up.2.dept <- FALSE
+if( ! exists("do.predictions")) do.predictions <- TRUE
+
+models.run0 <- list()
+for(i in unique(models.run$risk)){
+  models.run0[[i]] <- models.run$lst[models.run$risk == i]
 }
-if( 'mr' %in% models.run$risk ){
-  med.risk.fires  <- dbGetQuery(conn, "select * from nist.med_risk_fires" )
-  med.risk.fires  <- fcSetup(med.risk.fires )
-  lr.mr.pred <- dbGetQuery(conn, "select * from nist.lr_mr_pred")
-  lr.mr.pred <- fcSetup(lr.mr.pred)
-}
-if( 'hr' %in% models.run$risk ){
-  high.risk.fires <- dbGetQuery(conn, "select * from nist.high_risk_fires")
-  high.risk.fires <- fcSetup(high.risk.fires)
-  hr.pred    <- dbGetQuery(conn, "select * from nist.hr_pred")
-  hr.pred    <- fcSetup(hr.pred)
-}
-ctls <- models.run$lst
-# mass.npt needs to be modified. Instead of working with a pattern,
-# I need to work with a list. Probably the best way to handle it is
-# to make both options available.
-ctls <- mass.npt(list=ctls)
-objs <- fcMacro(ctls)
-# Note that most of these will NOT run the 'fcTest' routine because 
-# the target variables will differ within the object. For this
-# stage, the fact that it does not run 'fcTest' is not important.
-#
-# OK. This doesn't work. The problem is I need the names of the input and
-# output objects as well as the associated prediction data frame.
-# The solution, I think, is to supply a data.frame with all that information.
-# That will result in a slight modification of the work above, where I do 
-# mass.npt for each object rather than by the approach I use above.
-#
-# The other problem is knowing what input files go with what estimation file.
-# I can either hard-code that information in or set up the routine so that the
-# information is supplied. I think that the latter is the best approach. I am
-# already building a data frame with the list of control objects to be run. It 
-# would be a simple matter to include the .pred table to estimate with it as a 
-# column.
-objs <- merge(objs, models.run, by.x=c("npt.name"), by.y="npt")
-for( i in 1:nrow(objs)){
-  e <- new.env()
-  load(objs$save.name[i], e)
-  if(objs$risk=="hr"){
-    uu <- fcEstimate( get(objs$npt.name[i], e), 
-                      get(objs$res.name[i], e), 
-                      hr.pred
-    )
-# Here is my planned work-plan for high-risk predictions.
-# First, isolate the index fields (except for 'parcel_id'), probably
-#   using intersect with the names of the fields.
-# Then list the prediction fields.
-# Aggregate, using the formula interface (which will require a 'formula(text)' type call)
-# 
-  ndx  <- intersect(c("year", "geoid", "state", "region", "fd_id", "fd_size"), names(uu))
-  vars <- setdiff(names(uu), c("year", "geoid", "geoid_source", "parcel_id", "state", "region", "fd_id", "fd_size"))
-  uu <- aggregate()
-  } else {
-    uu <- fcEstimate( get(objs$npt.name[i], e), 
-                      get(objs$res.name[i], e), 
-                      lr.mr.pred
-    )
+
+for(i in names(models.run0)){
+  src.name <- est.names[i]
+  if(! exists(est.name)){
+    assign(est.name, dbGetQuery(conn, est.tabls[i]))
+    assign(est.name, fcSetup(get(est.name))
   }
-  if(exists("results")){
-    results <- merge(results, uu)
-  } else {
-    results <- uu
+  models  <- mass.npt(conn, list=models.run0[[i]])
+  objects <- fcMacro(models)
+#
+# The format of 'objects' (from the fcMacro documentation) is:
+#     npt.name   res.name       tst.name       msg.name         save.name
+#     npt.final  npt.final.res  npt.final.tst  messages.00.txt  npt.final.RData
+#     ...
+#
+  if(do.predictions){
+    est.name <- src.names[i]
+    if(! exists(src.name)){
+      assign(src.name, dbGetQuery(conn, src.tabls[i]))
+      assign(src.name, fcSetup(get(src.name))
+    }
+# Basically what I am going to do here is call fcEstimate. Note that 
+# fcEstimate can take lists of control and output objects and work on 
+# them as a unit. However, since the output objects can be quite large,
+# I am doing them one at a time so as not to blow up the machine.
+    for(j in 1:nrow(objects)){
+      e <- new.env()
+      load(objects$save.name[i], e)
+      with(objects, assign(npt.name[i], get(npt.name[i], e)))
+      with(objects, assign(res.name[i], get(res.name[i], e)))
+      rm(e)
+      temp.98765 <- fcEstimate(objects$npt.name[i], 
+                               objects$res.name[i], 
+                               get("est.name"), 
+                               subset=quote(fd_size %in% paste("size_", 3:9, sep="")))
+# As written it is possible for some results to have the census tract column called
+# 'geoid' in some cases and 'tr10_fid' in others. More particularly, the team wants
+# the column called 'tr10_fid' to be consistent with their work. This takes care of 
+# that.
+      if( "geoid" %in% names(temp.98765)){
+        names(temp.98765)[match("geoid", names(temp.98765))] <- "tr10_fid"
+      }
+# Here I get a list of the names of the prediction columns.
+      dta.cols <- setdiff(names(temp.98765), c("geoid", "tr10_fid", "fd_id", "parcel_id", "year", "geoid_source"))
+# Since the high-risk results are at the parcel level, and all the rest of the data
+# is at the census tract level, I need to roll up the hr data to the census tract level.
+      if(i == 'hr'){
+        temp.98765 <- aggregate(temp.98765[, dta.cols], 
+                                with(temp.98765, list(year=year, geoid=geoid, fd_id=fd_id), 
+                                function(x) sum(x, na.rm=TRUE)))
+      }
+# Since it is highly likely that there will be multiple columns across different
+# risk classes with the same names, take steps to make the names different.
+      names(temp.98765)[match(dta.cols), names(temp.98765)] <- paste(i, dta.cols, sep=".")
+# Combine the various predictions into a single data frame.
+      if(exists("predictions")){
+        predictions <- merge(predictions, temp.98765, all=TRUE)
+      } else {
+        predictions <- temp.98765
+      }
+      rm(temp.98765)
+    }
+# If requested, roll the data up to the department level.
+    if(roll.up.2.dept){
+      dta.cols <- setdiff(names(predictions), c("geoid", "tr10_fid", "fd_id", "parcel_id", "year", "geoid_source"))
+      temp.98765 <- aggregate(predictions[, dta.cols], 
+                              with(predictions, list(year=year, fd_id=fd_id), 
+                              function(x) sum(x, na.rm=TRUE)))	  
+    }
   }
 }
