212c4e3b3c86ce91cf45672b7a4d5585b8a9e1ed
Stanley Gilbert <stanley.gilbert@nist.gov>
2017-09-11 16:34:37 -0400

    Obsolete and deleted

--- r_code/models_run.R
+++
@@ -1,8 +0,0 @@
-models.run <- matrix( c("lr", "npt.final",
-                        "lr", "npt.final.L",
-                        "mr", "mr.final",
-                        "hr", "hr.final"     ),
-                      byrow=TRUE,
-                      ncol=2 )
-models.run <- as.data.frame(models.run, stringsAsFactors=FALSE)
-names(models.run) <- c("risk", "lst")
