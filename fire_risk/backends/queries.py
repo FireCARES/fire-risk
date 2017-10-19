b68c084c534336934eb2c7e4e41e666031e549dc
Isaac Torres <istodi@gmail.com>
2017-10-19 14:59:21 -0500

    update queries to use department NFIRS data

--- fire_risk/backends/queries.py
+++ fire_risk/backends/queries.py
@@ -1,17 +1,17 @@
 # To create the buildingfires table from the NFIRS database use this script:
 # https://github.com/FireCARES/data/blob/master/sources/nfirs/scripts/building_fires.sql
 
-ALL_BUILDING_FIRES = 'SELECT fire_sprd, COUNT(*) FROM buildingfires WHERE fire_sprd IN (\'1\',\'2\',\'3\',' \
+ALL_BUILDING_FIRES = 'SELECT fire_sprd, COUNT(*) FROM joint_buildingfires WHERE fire_sprd IN (\'1\',\'2\',\'3\',' \
                      '\'4\',\'5\') GROUP BY fire_sprd ORDER BY fire_sprd DESC;'
 
-BUILDING_FIRES_BY_FDID_STATE = 'SELECT fire_sprd, COUNT(*) FROM buildingfires WHERE fire_sprd IN (\'1\',\'2\',' \
+BUILDING_FIRES_BY_FDID_STATE = 'SELECT fire_sprd, COUNT(*) FROM joint_buildingfires WHERE fire_sprd IN (\'1\',\'2\',' \
                                   '\'3\',\'4\',\'5\') AND fdid=(%s) AND state=(%s)  GROUP BY fire_sprd ORDER BY' \
                                   ' fire_sprd DESC;'
 
 
 ALL_RESIDENTIAL_FIRES = """
 SELECT fire_sprd, COUNT(*)
-FROM buildingfires
+FROM joint_buildingfires
 WHERE prop_use IN ('419','429','439','449','459','460','462','464','400') AND fire_sprd IN ('1','2','3','4','5')
 GROUP BY fire_sprd
 ORDER BY fire_sprd;
@@ -20,7 +20,7 @@ ORDER BY fire_sprd;
 
 RESIDENTIAL_FIRES_BY_FDID_STATE = """
 SELECT fdid, fire_sprd, COUNT(*)
-FROM buildingfires
+FROM joint_buildingfires
 WHERE prop_use IN ('419','429','439','449','459','460','462','464','400')
     AND fire_sprd IN ('1','2','3','4','5')
     AND fdid=(%s) AND state=(%s)
@@ -33,7 +33,7 @@ RESIDENTIAL_FIRES_BY_FDID_STATE_HAZARD = """
 SELECT *
     FROM crosstab(
       'select COALESCE(y.risk_category, ''N/A'') as risk_category, fire_sprd, count(*)
-        FROM buildingfires a left join (
+        FROM joint_buildingfires a left join (
           SELECT state,
             fdid,
             inc_date,
@@ -42,7 +42,7 @@ SELECT *
             geom,
             x.parcel_id,
             x.risk_category
-          FROM (select * from incidentaddress a
+          FROM (select * from joint_incidentaddress a
              left join parcel_risk_category_local b using (parcel_id)
              ) AS x
         ) AS y using (state, inc_date, exp_no, fdid, inc_no)
