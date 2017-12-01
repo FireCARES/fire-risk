b6fa0446c00ee50079b0a0a682b11ea226cb43d9
Joe Meilinger <joemeilinger@gmail.com>
2017-12-01 11:31:34 -0600

    Added test for including response times + hazard levels

--- tests/models/tests.py
+++ tests/models/tests.py
@@ -1,8 +1,9 @@
 import unittest
+import pytest
 from mock import patch
 from fire_risk import backends
 from fire_risk.backends.queries import RESIDENTIAL_FIRES_BY_FDID_STATE
-from fire_risk.models import DIST, DISTMediumHazard, DISTHighHazard
+from fire_risk.models import DIST, DISTMediumHazard, DISTHighHazard, NotEnoughRecords
 from fire_risk.models.DIST.providers.ahs import ahs_building_areas
 from fire_risk.models.DIST.providers.iaff import response_time_distributions
 from fire_risk.utils import LogNormalDraw
@@ -162,6 +163,71 @@ class TestDISTModel(unittest.TestCase):
             score = dist(floor_extent=False, **res).gibbs_sample()
 
             self.assertAlmostEqual(score, expected[k], delta=3)
+
+    @patch('fire_risk.backends.psycopg2')
+    def test_pg_hazard_response_time_distribution(self, psycopg2):
+        fd = ('AW824', 'TX')
+        mock_cur = psycopg2.connect.return_value.cursor.return_value
+        res = [{'risk_category': 'High',
+                'object_of_origin': 13L,
+                'room_of_origin': 26L,
+                'floor_of_origin': 4L,
+                'building_of_origin': 13L,
+                'beyond': 2L
+                },
+               {'risk_category': 'Low',
+                'object_of_origin': 216L,
+                'room_of_origin': 278L,
+                'floor_of_origin': 94L,
+                'building_of_origin': 258L,
+                'beyond': 41L
+                },
+               {'risk_category': 'Medium',
+                'object_of_origin': 141L,
+                'room_of_origin': 138L,
+                'floor_of_origin': 37L,
+                'building_of_origin': 107L,
+                'beyond': 12L
+                },
+               {'risk_category': 'N/A',
+                'object_of_origin': 241L,
+                'room_of_origin': 341L,
+                'floor_of_origin': 89L,
+                'building_of_origin': 313L,
+                'beyond': 58L
+                }]
+
+        mock_cur.fetchall.return_value = res
+
+        expected = {'High': None,
+                    'Medium': 38,
+                    'Low': 55,
+                    'N/A': 57}
+
+        with backends.PostgresBackend(dict(service='nfirs')) as backend:
+            results = backend.get_hazard_level_firespread_counts(query_params=fd)
+
+        for k in results:
+            res = results[k]
+            res['building_area_draw'] = ahs_building_areas(*fd)
+            response_times = response_time_distributions.get('{0}-{1}'.format(*fd))
+            if response_times:
+                res['arrival_time_draw'] = LogNormalDraw(*response_times, multiplier=60)
+
+            if k == 'High':
+                dist = DISTHighHazard
+            elif k == 'Medium':
+                dist = DISTMediumHazard
+            else:
+                dist = DIST
+
+            if expected[k] is not None:
+                score = dist(floor_extent=False, **res).gibbs_sample()
+                self.assertAlmostEqual(score, expected[k], delta=3)
+            else:
+                with pytest.raises(NotEnoughRecords):
+                    dist(floor_extent=False, **res).gibbs_sample()
+
 
 
 if __name__ == '__main__':
